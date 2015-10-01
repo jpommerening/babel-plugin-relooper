9126113a9f4809b90046cef5c69cc5d3ce40273a
Jonas Pommerening <jonas.pommerening@gmail.com>
2015-10-01 12:39:21 +0200

    Refactor, fix stuff

--- src/index.js
+++ src/index.js
@@ -1,33 +1,42 @@
+import { types, traverse } from 'babel-core';
 
-export function extractBody( fn, scope = fn.scope ) {
-  var body = [];
-  var id;
+export function extractBody(fn, scope = fn.scope, visitor) {
+  const body = [];
+  const args = [];
 
-  for (var name in fn.scope.bindings) {
-    if (fn.scope.bindings[ name ].scope === fn.scope) {
-      id = scope.generateUidIdentifier(name);
-      fn.scope.rename( name, id.name );
-      scope.push( { id } );
-    }
-  }
+  fn.get('body').traverse(Object.assign({}, visitor, {
+    Identifier(node, parent, s) {
+      if (fn.scope.hasOwnBinding( node.name )) {
+        let name = node.name;
+        let id = scope.generateUid(name);
 
-  fn.get('body').traverse({
-    ReturnStatement(node, parent) {
-      this.replaceWithMultiple([
-        t.expressionStatement(t.assignmentExpression('=', t.memberExpression(id, index, true), node.argument)),
-        t.continueStatement()
-      ]);
-    },
-    ThisExpression(node) {
-      if (args[1] && !ref) {
-        ref = declare(scope.generateUidIdentifier('this'), args[1].node);
+        if (fn.scope.getOwnBinding( name ).kind === 'param') {
+          args.push( id );
+        }
+
+        fn.scope.rename( name, id );
+        fn.scope.moveBindingTo( id, scope );
+        scope.push( { id: node } );
       }
-      return ref || node;
+
+      traverse(parent, visitor, s, this.state, this.parentPath);
     },
-    Statement(node, parent) {
-      body.push(node);
+    'AssignmentExpression|UpdateExpression'(node, parent, s) {
+      var left = node.left;
+      var binding = scope.getOwnBinding( left.name );
+
+      if (args.indexOf( left.name ) >= 0) {
+        //console.log('warning: modifying parameter value!');
+      }
+
+      traverse(parent, visitor, s, this.state, this.parentPath);
+    },
+    Statement: {
+      exit(node, parent) {
+        body.push(node);
+      }
     }
-  }, scope);
+  }), scope);
 
 
   return body;
@@ -35,13 +44,8 @@ export function extractBody( fn, scope = fn.scope ) {
 
 export default function ({ Plugin, types: t }) {
 
-  return new Plugin("reloop", {
+  return new Plugin('relooper', {
     visitor: {
-      Function: {
-        enter() {
-          this.skip();
-        }
-      },
       CallExpression: {
         exit(node, parent, scope) {
           var callee = this.get('callee');
@@ -57,77 +61,52 @@ export default function ({ Plugin, types: t }) {
             if (callee.get('property').isIdentifier({ name: 'map' })) {
               var id = scope.generateUidIdentifierBasedOnNode(node);
               var array = callee.get('object').node;
-              var currentValue;
-              var index;
 
               var ref;
               var decls = [
                 t.variableDeclarator(t.identifier(id.name), t.arrayExpression([]))
               ];
-              var body = [
-              ];
 
               id.typeAnnotation = t.genericTypeAnnotation({ type: 'Identifier', name: 'Array'});
 
-              function declare( identifier, value ) {
-                decls.push(t.variableDeclarator(identifier, value));
-                return identifier;
+              if (params.length < 1) {
+                params[0] = scope.generateDeclaredUidIdentifier('currentValue');
               }
 
-              /*
-              for (var name in fn.scope.bindings) {
-                if (fn.scope.bindings[ name ].scope === fn.scope) {
-                  fn.scope.rename( name, declare(scope.generateUidIdentifier(name)).name );
-                }
-              }
-              */
-
-              if (params.length > 0) {
-                currentValue = params[0];
-              } else {
-                currentValue = declare(scope.generateUidIdentifier('currentValue'));
+              if (params.length < 2) {
+                params[1] = scope.generateDeclaredUidIdentifier('index');
               }
 
-              if (params.length > 1) {
-                index = params[1];
-              } else {
-                index = declare(scope.generateUidIdentifier('index'));
-              }
+              const body = extractBody(fn, scope, {
+                ReturnStatement(node, parent) {
+                  return [
+                    t.expressionStatement(t.assignmentExpression('=', t.memberExpression(id, params[1], true), node.argument)),
+                    t.continueStatement()
+                  ];
+                },
+                ThisExpression(node) {
+                  if (args[1] && !ref) {
+                    ref = scope.generateUidIdentifier('this');
+                    scope.push( { id: ref });
+                    decls.push(t.variableDeclarator(ref, args[1].node));
+                  }
+                  return ref || node;
+                },
+              });
 
               if (params.length > 2) {
                 fn.scope.rename( params[2].name, array.name );
               }
 
-              /*
-              fn.get('body').traverse({
-                ReturnStatement(node, parent) {
-                  this.replaceWithMultiple([
-                    t.expressionStatement(t.assignmentExpression('=', t.memberExpression(id, index, true), node.argument)),
-                    t.continueStatement()
-                  ]);
-                },
-                ThisExpression(node) {
-                  if (args[1] && !ref) {
-                    ref = declare(scope.generateUidIdentifier('this'), args[1].node);
-                  }
-                  return ref || node;
-                },
-                Statement(node, parent) {
-                  body.push(node);
-                }
-              }, scope);
-              */
-             body = body.concat( extractBody( fn, scope ) );
-
               this.getStatementParent().insertBefore([
                 t.variableDeclaration('var', decls),
                 t.forStatement(
-                  t.assignmentExpression('=', currentValue, t.memberExpression(array,
-                    t.assignmentExpression('=', index, t.literal(0)),
+                  t.assignmentExpression('=', params[0], t.memberExpression(array,
+                    t.assignmentExpression('=', params[1], t.literal(0)),
                     true)),
-                  t.binaryExpression('<', index, t.memberExpression(array, t.identifier('length'))),
-                  t.assignmentExpression('=', currentValue, t.memberExpression(array,
-                    t.updateExpression('++', index, true),
+                  t.binaryExpression('<', params[1], t.memberExpression(array, t.identifier('length'))),
+                  t.assignmentExpression('=', params[0], t.memberExpression(array,
+                    t.updateExpression('++', params[1], true),
                     true)),
                   t.blockStatement(body)
                 )
